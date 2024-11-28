27c226e8a9300c47d63a70d32d83b79ed3684cca
Divyansh1234567890 <divyanshsharma151020@gmail.com>
2024-11-28 19:23:44 +0000

    customRender

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,23 @@
-
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM  from 'react-dom/client'
 
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom App | chai</h1>
+        </div>
+    )
+}
+ const anotherUser = 'chai aur code'
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com',target: '_blank'},
+    'click me to visit google',
+    anotherUser
+)
 
-    <App />
+ReactDOM.createRoot(document.getElementById('root')).render(
+    reactElement
 )
