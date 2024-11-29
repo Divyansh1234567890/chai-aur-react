e9485744fc2085d270c2cad4048f67e95a16b68f
Divyansh1234567890 <divyanshsharma151020@gmail.com>
2024-11-29 17:09:10 +0000

    hooksInReact

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter,setCounter] = useState(0);
+  const addValue = ()=>{
+    if(counter<=18)
+    setCounter(counter+1);
+  }
+  const removeValue =()=>{
+    if(counter>=0)
+    setCounter(counter-1);
+  }
+  return (
+   <>
+   <h1>Chai aur react</h1>
+   <h2>counter value:{counter+1}</h2>
+   <button
+    onClick={addValue}>
+    Add value{counter+1}</button>
+   <br></br>
+   <button
+   onClick={removeValue}>
+   Remove value{counter+1}
+   </button>
+   </>
+  )
+}
+
+export default App
